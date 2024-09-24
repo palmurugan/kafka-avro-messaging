0a5e0556c5ea50f67a2a2aff5baa0e97dc850603
palmurugan <palmurugan.c@gmail.com>
2024-09-25 00:24:57 +0530

    Avro Generator Added

---
+++ src/main/java/com/pal/poc/kafka/avro/KafkaAvroMessagingApplication.java
@@ -0,0 +1,13 @@
+package com.pal.poc.kafka.avro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaAvroMessagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaAvroMessagingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pal/poc/kafka/avro/KafkaAvroMessagingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pal.poc.kafka.avro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaAvroMessagingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
